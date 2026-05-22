DevExpress-Examples/devextreme-datagrid-custom-sorting-grouped-column

Update Angular to use standalone components (#49)

Angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DxButtonModule } from 'devextreme-angular/ui/button';
 import { ClickEvent } from 'devextreme/ui/button';
 
 @Component({
     selector: 'app-root',
+    imports: [DxButtonModule],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
 })
 export class AppComponent {
   title = 'Angular';

DevExpress-Examples/devextreme-getting-started-with-diagram

Angular/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { DxDiagramModule } from 'devextreme-angular/ui/diagram';
 import { DiagramNode, DiagramEdge } from './app.types';
 
 @Component({
   selector: 'app-root',
+  imports: [DxDiagramModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })