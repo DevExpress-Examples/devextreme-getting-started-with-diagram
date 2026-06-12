4ef9b73417fa2d3ebc1d34cb706d91dfe0e4d12c
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 13:35:17 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { DxDiagramModule } from 'devextreme-angular/ui/diagram';
 import { DiagramNode, DiagramEdge } from './app.types';
 
@@ -6,6 +6,7 @@ import { DiagramNode, DiagramEdge } from './app.types';
   selector: 'app-root',
   imports: [DxDiagramModule],
   templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
